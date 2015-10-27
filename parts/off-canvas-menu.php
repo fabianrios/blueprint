8fa2ae1daa38633980ea9fa1ac73ebdb59c58839
fabianrios <hola@fabianrios.co>
2015-10-26 20:50:49 -0500

    menu movil

--- parts/off-canvas-menu.php
+++ parts/off-canvas-menu.php
@@ -10,25 +10,30 @@
 ?>
 
 <div class="nav-header">
-	<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/logo_moviles.jpg" alt="" />
+	<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/logo_movil.jpg" alt="" />
 	<a href="#" class="tree right" data-reveal-id="menu"><img src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/tree.jpg" alt="" /></a>
 </div>
 
 <div id="menu" class="reveal-modal blueish" data-reveal aria-labelledby="modalTitle" aria-hidden="true" role="dialog">
   <div class="nav-header">
-  	<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/logo_moviles.jpg" alt="" />
+  	<img src="<?php bloginfo('stylesheet_directory'); ?>/assets/images/logo_movil.jpg" alt="" />
   	<a class="close-reveal-modal" aria-label="Close">&#215;</a>
   </div>
   
-  <ul>
-  	<li><a href="">STRATEGY</a></li>
-	<li><a href="">ABOUT US</a></li>
-	<li><a href="">CONTACT US</a></li>
-	<li><a href="">PORTFOLIO</a></li>
-  </ul>
-  <hr />
-  <ul>
-  	<li><a href="#">CLIENT LOGIN</a></li>
-  </ul>
+ <div class="row">
+ 	<div class="small-8 small-centered columns">
+		<br /><br /><br />
+ 		 <ul>
+ 			<li><a href="">STRATEGY</a></li>
+ 			<li><a href="">ABOUT US</a></li>
+ 			<li><a href="">CONTACT US</a></li>
+ 			<li><a href="">PORTFOLIO</a></li>
+ 		</ul>
+ 		<hr />
+ 		<ul>
+ 			<li><a href="#">CLIENT LOGIN</a></li>
+ 		</ul>
+ 	</div>
+ </div>
   
 </div>
